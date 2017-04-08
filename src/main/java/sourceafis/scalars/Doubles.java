82e56013cbf295061e68492a502bf6d76291af82
Robert Vazan <robvcode@gmail.com>
2017-04-08 18:11:05 +0200

    Dropped small type classes

--- src/main/java/sourceafis/scalars/Doubles.java
+++ src/main/java/sourceafis/scalars/Doubles.java
@@ -7,10 +7,10 @@ public class Doubles {
 	public static double interpolate(double start, double end, double position) {
 		return start + position * (end - start);
 	}
-	public static double interpolate(double topleft, double topright, double bottomleft, double bottomright, FPoint position) {
-		double left = interpolate(bottomleft, topleft, position.y);
-		double right = interpolate(bottomright, topright, position.y);
-		return interpolate(left, right, position.x);
+	public static double interpolate(double topleft, double topright, double bottomleft, double bottomright, double x, double y) {
+		double left = interpolate(bottomleft, topleft, y);
+		double right = interpolate(bottomright, topright, y);
+		return interpolate(left, right, x);
 	}
 	public static double interpolateExponential(double start, double end, double position) {
 		return Math.pow(end / start, position) * start;
