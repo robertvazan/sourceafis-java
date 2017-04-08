f24b47ecee9f691a814e07341271649b9d6055b2
Robert Vazan <robvcode@gmail.com>
2017-04-08 19:52:15 +0200

    Beginning of FingerprintTemplate

---
+++ src/main/java/sourceafis/FingerprintTemplate.java
@@ -0,0 +1,48 @@
+package sourceafis;
+
+import sourceafis.collections.*;
+
+public class FingerprintTemplate {
+	public FingerprintTemplate(DoubleMap image) {
+		this(image, 500);
+	}
+	public FingerprintTemplate(DoubleMap image, double dpi) {
+		final int blockSize = 15;
+		final double dpiTolerance = 10;
+		if (Math.abs(dpi - dpiTolerance) > 10)
+			image = scaleImage(image, dpi);
+		BlockMap blocks = new BlockMap(image.width, image.height, blockSize);
+	}
+	static DoubleMap scaleImage(DoubleMap input, double dpi) {
+		return scaleImage(input, (int)Math.round(500.0 / dpi * input.width), (int)Math.round(500.0 / dpi * input.height));
+	}
+	static DoubleMap scaleImage(DoubleMap input, int newWidth, int newHeight) {
+		DoubleMap output = new DoubleMap(newWidth, newHeight);
+		double scaleX = newWidth / (double)input.width;
+		double scaleY = newHeight / (double)input.height;
+		double descaleX = 1 / scaleX;
+		double descaleY = 1 / scaleY;
+		for (int y = 0; y < newHeight; ++y) {
+			double y1 = y * descaleY;
+			double y2 = y1 + descaleY;
+			int y1i = (int)y1;
+			int y2i = (int)Math.ceil(y2);
+			for (int x = 0; x < newWidth; ++x) {
+				double x1 = x * descaleX;
+				double x2 = x1 + descaleX;
+				int x1i = (int)x1;
+				int x2i = (int)Math.ceil(x2);
+				double sum = 0;
+				for (int oy = y1i; oy < y2i; ++oy) {
+					double ry = Math.min(oy + 1, y2) - Math.max(oy, y1);
+					for (int ox = x1i; ox < x2i; ++ox) {
+						double rx = Math.min(ox + 1, x2) - Math.max(ox, x1);
+						sum += rx * ry * input.get(ox, oy);
+					}
+				}
+				output.set(x, y, sum * (scaleX * scaleY));
+			}
+		}
+		return output;
+	}
+}
