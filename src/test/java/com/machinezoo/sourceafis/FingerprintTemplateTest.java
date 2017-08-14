acbe61b1256e9a51149de5fa9262228f7fe48f75
Robert Vazan <robvcode@gmail.com>
2017-08-14 10:47:25 +0200

    Tests for image decoding

---
+++ src/test/java/com/machinezoo/sourceafis/FingerprintTemplateTest.java
@@ -0,0 +1,44 @@
+package com.machinezoo.sourceafis;
+
+import static org.junit.Assert.*;
+import java.io.*;
+import org.apache.commons.io.*;
+import org.junit.*;
+import com.machinezoo.sourceafis.internal.*;
+import lombok.*;
+
+public class FingerprintTemplateTest {
+	@Test public void readImage_png() {
+		validate(FingerprintTemplate.readImage(load("probe.png")));
+	}
+	@Test public void readImage_jpeg() {
+		validate(FingerprintTemplate.readImage(load("probe.jpeg")));
+	}
+	@Test public void readImage_bmp() {
+		validate(FingerprintTemplate.readImage(load("probe.bmp")));
+	}
+	@Test public void readImage_tiff() {
+		validate(FingerprintTemplate.readImage(load("probe.tiff")));
+	}
+	private void validate(DoubleMap map) {
+		assertEquals(388, map.width);
+		assertEquals(374, map.height);
+		DoubleMap reference = FingerprintTemplate.readImage(load("probe.png"));
+		double delta = 0, max = -1, min = 1;
+		for (int x = 0; x < map.width; ++x) {
+			for (int y = 0; y < map.height; ++y) {
+				delta += Math.abs(map.get(x, y) - reference.get(x, y));
+				max = Math.max(max, map.get(x, y));
+				min = Math.min(min, map.get(x, y));
+			}
+		}
+		assertTrue(max > 0.9);
+		assertTrue(min < 0.1);
+		assertTrue(delta / (map.width * map.height) < 0.01);
+	}
+	@SneakyThrows private byte[] load(String name) {
+		try (InputStream input = FingerprintTemplateTest.class.getResourceAsStream("/com/machinezoo/sourceafis/" + name)) {
+			return IOUtils.toByteArray(input);
+		}
+	}
+}
