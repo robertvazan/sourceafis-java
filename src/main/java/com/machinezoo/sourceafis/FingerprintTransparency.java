8038dd48f3976bfdd089d7456a38fa91d137e5d7
Robert Vazan <robvcode@gmail.com>
2019-10-15 09:39:32 +0000

    Moved TransparencyZip into FingerprintTransparency

--- src/main/java/com/machinezoo/sourceafis/FingerprintTransparency.java
+++ src/main/java/com/machinezoo/sourceafis/FingerprintTransparency.java
@@ -1,12 +1,16 @@
 // Part of SourceAFIS: https://sourceafis.machinezoo.com
 package com.machinezoo.sourceafis;
 
+import static java.util.stream.Collectors.*;
 import java.io.*;
 import java.nio.*;
+import java.nio.channels.*;
 import java.nio.charset.*;
 import java.util.*;
 import java.util.function.*;
+import java.util.zip.*;
 import com.google.gson.*;
+import com.machinezoo.noexception.*;
 import gnu.trove.map.hash.*;
 
 /**
@@ -124,6 +128,38 @@ public abstract class FingerprintTransparency implements AutoCloseable {
 	 */
 	public static FingerprintTransparency zip(OutputStream stream) {
 		return new TransparencyZip(stream);
+	}
+	private static class TransparencyZip extends FingerprintTransparency {
+		private final ZipOutputStream zip;
+		private int offset;
+		TransparencyZip(OutputStream stream) {
+			zip = new ZipOutputStream(stream);
+		}
+		@Override protected void log(String keyword, Map<String, Supplier<ByteBuffer>> data) {
+			Exceptions.sneak().run(() -> {
+				List<String> suffixes = data.keySet().stream()
+					.sorted(Comparator.comparing(ext -> {
+						if (ext.equals(".json"))
+							return 1;
+						if (ext.equals(".dat"))
+							return 2;
+						return 3;
+					}))
+					.collect(toList());
+				for (String suffix : suffixes) {
+					++offset;
+					zip.putNextEntry(new ZipEntry(String.format("%03d", offset) + "-" + keyword + suffix));
+					ByteBuffer buffer = data.get(suffix).get();
+					WritableByteChannel output = Channels.newChannel(zip);
+					while (buffer.hasRemaining())
+						output.write(buffer);
+					zip.closeEntry();
+				}
+			});
+		}
+		@Override public void close() {
+			Exceptions.sneak().run(zip::close);
+		}
 	}
 	/*
 	 * To avoid null checks everywhere, we have one noop class as a fallback.
