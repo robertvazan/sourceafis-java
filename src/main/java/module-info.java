fddf3f89617db9d486d58ffea3bea6e812b63100
Robert Važan <robert.vazan@tutanota.com>
2021-12-24 19:38:40 +0000

    No copyright footer in javadoc

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,9 +5,9 @@
  */
 module com.machinezoo.sourceafis {
 	exports com.machinezoo.sourceafis;
+	requires java.desktop;
 	requires com.machinezoo.noexception;
 	requires transitive com.machinezoo.fingerprintio;
-	requires java.desktop;
 	requires com.fasterxml.jackson.core;
 	requires com.fasterxml.jackson.annotation;
 	requires com.fasterxml.jackson.databind;
