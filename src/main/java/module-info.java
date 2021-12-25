577635d2b267588c0bc7841a9343420719d7160e
Robert Važan <robert.vazan@tutanota.com>
2021-12-25 02:23:52 +0000

    Cleanup in module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,17 +5,26 @@
  */
 module com.machinezoo.sourceafis {
 	exports com.machinezoo.sourceafis;
+	/*
+	 * We only need ImageIO from the whole desktop module.
+	 */
 	requires java.desktop;
 	requires com.machinezoo.noexception;
 	requires transitive com.machinezoo.fingerprintio;
-	requires com.fasterxml.jackson.core;
-	requires com.fasterxml.jackson.annotation;
 	requires com.fasterxml.jackson.databind;
 	requires com.fasterxml.jackson.dataformat.cbor;
+	/*
+	 * Gson is only used by deprecated JSON serialization of templates.
+	 */
 	requires com.google.gson;
 	requires it.unimi.dsi.fastutil;
 	requires org.apache.commons.io;
+	/*
+	 * JNBIS is not packaged as module yet.
+	 */
 	requires jnbis;
-	requires org.slf4j;
+	/*
+	 * Serialization needs reflection access.
+	 */
 	opens com.machinezoo.sourceafis to com.fasterxml.jackson.databind, com.google.gson;
 }
