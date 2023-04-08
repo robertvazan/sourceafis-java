04942ff1984f92dae7ca12f3b9f16a489dde159d
Robert Važan <robert.vazan@tutanota.com>
2023-04-08 11:39:05 +0000

    Updated project configuration

--- scripts/configure.py
+++ scripts/configure.py
@@ -1,45 +1,39 @@
 # This script generates and updates project configuration files.
 
-# We are assuming that project-config is available in sibling directory.
-# Checkout from https://github.com/robertvazan/project-config
-import pathlib
-project_directory = lambda: pathlib.Path(__file__).parent.parent
-config_directory = lambda: project_directory().parent/'project-config'
-exec((config_directory()/'src'/'java.py').read_text())
+# Run this script with rvscaffold in PYTHONPATH
+import rvscaffold as scaffold
 
-project_script_path = __file__
-repository_name = lambda: 'sourceafis-java'
-pretty_name = lambda: 'SourceAFIS for Java'
-pom_subgroup = lambda: 'sourceafis'
-pom_artifact = lambda: 'sourceafis'
-pom_name = lambda: 'SourceAFIS'
-pom_description = lambda: 'Fingerprint recognition engine that takes a pair of human fingerprint images and returns their similarity score. Supports efficient 1:N search.'
-inception_year = lambda: 2009
-homepage = lambda: website() + 'java'
-jdk_version = lambda: 11
-stagean_annotations = lambda: True
-project_status = lambda: stable_status()
+class Project(scaffold.Java):
+    def script_path_text(self): return __file__
+    def repository_name(self): return 'sourceafis-java'
+    def pretty_name(self): return 'SourceAFIS for Java'
+    def pom_name(self): return 'SourceAFIS'
+    def pom_description(self): return 'Fingerprint recognition engine that takes a pair of human fingerprint images and returns their similarity score. Supports efficient 1:N search.'
+    def inception_year(self): return 2009
+    def homepage(self): return self.website() + 'java'
+    def stagean_annotations(self): return True
+    def project_status(self): return self.stable_status()
+    
+    def dependencies(self):
+        yield from super().dependencies()
+        yield self.use('com.machinezoo.fingerprintio:fingerprintio:1.3.0')
+        yield self.use_fastutil()
+        yield self.use_commons_io()
+        yield self.use_gson()
+        yield from self.use_jackson_cbor()
+        yield self.use('com.github.mhshams:jnbis:2.1.1')
+        yield self.use_junit()
+        yield self.use_hamcrest()
+    
+    def javadoc_links(self):
+        yield from super().javadoc_links()
+        yield 'https://noexception.machinezoo.com/javadoc/'
+        yield 'https://fingerprintio.machinezoo.com/javadoc/'
+    
+    def documentation_links(self):
+        yield 'SourceAFIS for Java', self.homepage()
+        yield 'Javadoc', self.javadoc_home()
+        yield 'SourceAFIS overview', 'https://sourceafis.machinezoo.com/'
+        yield 'Algorithm', 'https://sourceafis.machinezoo.com/algorithm'
 
-def dependencies():
-    use('com.machinezoo.fingerprintio:fingerprintio:1.3.0')
-    use_fastutil()
-    use_commons_io()
-    use_gson()
-    use_jackson_cbor()
-    use('com.github.mhshams:jnbis:2.1.1')
-    use_junit()
-    use_hamcrest()
-
-javadoc_links = lambda: [
-    *standard_javadoc_links(),
-    'https://noexception.machinezoo.com/javadoc/',
-    'https://fingerprintio.machinezoo.com/javadoc/'
-]
-
-def documentation_links():
-    yield 'SourceAFIS for Java', homepage()
-    yield 'Javadoc', javadoc_home()
-    yield 'SourceAFIS overview', 'https://sourceafis.machinezoo.com/'
-    yield 'Algorithm', 'https://sourceafis.machinezoo.com/algorithm'
-
-generate()
+Project().generate()
